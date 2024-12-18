e33b524f3eb26caca5b49a26a949db4a3ce09f5f
hornokattila <hornokattila@outlook.com>
2024-12-18 19:26:48 +0100

    Reduced images.

--- script/toggle.js
+++ script/toggle.js
@@ -1,4 +1,4 @@
-const images = ['img/tv_toggle_2.png', 'img/tv_toggle_1.png'];
+const images = ['img/tv_toggle_2.webp', 'img/tv_toggle_1.webp'];
 
 const imageElement = document.getElementById('toggleImage');
 

26bdefb2139b34add0d72f402f6ce6b8cb074952
hornokattila <hornokattila@outlook.com>
2025-02-20 23:32:12 +0100

    Fullscreen try 2.

--- script/landscape.js
+++ script/landscape.js
@@ -1,23 +1,26 @@
+function enableFullscreen() {
+    // Try to go fullscreen
+    let docEl = document.documentElement;
+    if (docEl.requestFullscreen) {
+        docEl.requestFullscreen();
+    } else if (docEl.mozRequestFullScreen) { // Firefox
+        docEl.mozRequestFullScreen();
+    } else if (docEl.webkitRequestFullscreen) { // Chrome, Safari, Opera
+        docEl.webkitRequestFullscreen();
+    } else if (docEl.msRequestFullscreen) { // IE/Edge
+        docEl.msRequestFullscreen();
+    }
+}
+
 function checkOrientation() {
     if (window.matchMedia("(orientation: landscape)").matches) {
-        // Try to go fullscreen
-        let docEl = document.documentElement;
-        if (docEl.requestFullscreen) {
-            docEl.requestFullscreen();
-        } else if (docEl.mozRequestFullScreen) { // Firefox
-            docEl.mozRequestFullScreen();
-        } else if (docEl.webkitRequestFullscreen) { // Chrome, Safari, Opera
-            docEl.webkitRequestFullscreen();
-        } else if (docEl.msRequestFullscreen) { // IE/Edge
-            docEl.msRequestFullscreen();
-        }
+        enableFullscreen();
     } else {
         // Exit fullscreen if not in landscape
-        if (document.fullscreenElement) {
+        if (document.fullscreenElement || document.webkitFullscreenElement) {
             document.exitFullscreen();
         }
     }
 }
 
 window.addEventListener("orientationchange", checkOrientation);
-window.addEventListener("resize", checkOrientation); // For extra reliability

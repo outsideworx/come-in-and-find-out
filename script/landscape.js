44f651fac9335fde2911c69756c8d5219aed0d7c
hornokattila <hornokattila@outlook.com>
2025-02-21 01:05:33 +0100

    Fullscreen try 6.

--- script/landscape.js
+++ script/landscape.js
@@ -1,34 +1,6 @@
-function enableFullscreen() {
-    // Try to go fullscreen
-    let docEl = document.documentElement;
-    if (docEl.requestFullscreen) {
-        docEl.requestFullscreen();
-    } else if (docEl.mozRequestFullScreen) { // Firefox
-        docEl.mozRequestFullScreen();
-    } else if (docEl.webkitRequestFullscreen) { // Chrome, Safari, Opera
-        docEl.webkitRequestFullscreen();
-    } else if (docEl.msRequestFullscreen) { // IE/Edge
-        docEl.msRequestFullscreen();
-    }
-
-    // Lock orientation if supported
-    if (screen.orientation && screen.orientation.lock) {
-        try {
-            screen.orientation.lock("landscape");
-        } catch (err) {
-            alert(err);
-        }
-    }
-}
-
 function checkOrientation() {
     if (window.matchMedia("(orientation: landscape)").matches) {
-        enableFullscreen();
-    } else {
-        // Exit fullscreen if not in landscape
-        if (document.fullscreenElement || document.webkitFullscreenElement) {
-            document.exitFullscreen();
-        }
+        window.location.href = "red_home.html"
     }
 }
 

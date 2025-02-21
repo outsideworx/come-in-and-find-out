7a9680b093b35d9b6da356c6a9636a4c83be33b9
hornokattila <hornokattila@outlook.com>
2025-02-21 22:21:32 +0100

    Fullscreen try -1.

--- script/orientation/landscape.js
+++ script/orientation/landscape.js
@@ -1,38 +1,6 @@
-function full_screen() {
-    let element = document.getElementById("image-container") || document.documentElement;
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-    }
-}
-
-function exit_full_screen() {
-    if (document.fullscreenElement) {
-        document.exitFullscreen();
-    } else if (document.mozFullScreenElement) {
-        document.mozCancelFullScreen();
-    } else if (document.webkitFullscreenElement) {
-        document.webkitExitFullscreen();
-    } else if (document.msFullscreenElement) {
-        document.msExitFullscreen();
-    }
-}
-
 function checkOrientation() {
-    const image = document.getElementById("image");
-    const link = document.getElementById("link");
     if (window.innerWidth > window.innerHeight) {
-        image.src = "../img_pages/login.webp";
-        link.addEventListener("click", full_screen);
-    } else {
-        image.src = "../img_pages/index.webp";
-        link.removeEventListener("click", full_screen);
-        exit_full_screen();
+        window.location.href = "red_home.html";
     }
 }
 

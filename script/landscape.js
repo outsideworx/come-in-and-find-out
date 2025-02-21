17511649354074f30ef941063c2c1a8a60959db5
hornokattila <hornokattila@outlook.com>
2025-02-21 02:22:13 +0100

    Fullscreen try 14.

--- script/landscape.js
+++ script/landscape.js
@@ -1,6 +1,33 @@
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+function enableFullscreen() {
+    // Try to go fullscreen
+    let docEl = document.documentElement;
+    if (docEl.requestFullscreen) {
+        docEl.requestFullscreen();
+    } else if (docEl.mozRequestFullScreen) { // Firefox
+        docEl.mozRequestFullScreen();
+    } else if (docEl.webkitRequestFullscreen) { // Chrome, Safari, Opera
+        docEl.webkitRequestFullscreen();
+    } else if (docEl.msRequestFullscreen) { // IE/Edge
+        docEl.msRequestFullscreen();
+    }
+
+    document.getElementById("iframe-container").style.display = "block";
+}
+
 function checkOrientation() {
-    if (window.innerWidth > window.innerHeight) {
-        window.location.href = "red_home.html"
+    if (isMobileDevice()) {
+        if (window.innerWidth > window.innerHeight) {
+            const image = document.getElementById("image");
+            const link = document.getElementById("link");
+            image.src = "../img_pages/login.png"
+            link.onclick = enableFullscreen;
+        }
+    } else {
+        document.getElementById("iframe-container").style.display = "block";
     }
 }
 

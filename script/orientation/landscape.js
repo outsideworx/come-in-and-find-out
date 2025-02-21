09325a6e9b67b619eff4a2db347b1fe2ca31f0bb
hornokattila <hornokattila@outlook.com>
2025-02-21 21:07:15 +0100

    Fullscreen try 21.

---
+++ script/orientation/landscape.js
@@ -0,0 +1,8 @@
+function checkOrientation() {
+    if (window.innerWidth > window.innerHeight) {
+        window.location.href = "red_home.html";
+    }
+}
+
+window.addEventListener("load", checkOrientation);
+window.addEventListener("resize", checkOrientation);

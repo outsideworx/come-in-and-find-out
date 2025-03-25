c8f8839369d6c1adffcc1cf6420bed3aff9ba763
hornokattila <hornokattila@outlook.com>
2025-03-25 14:47:54 +0100

    Set navi.

--- script/fetch/categories.js
+++ script/fetch/categories.js
@@ -1,11 +1,15 @@
-function loadImages(category, offset) {
+function loadImages(category) {
+    const urlParams = new URLSearchParams(window.location.search);
+    let offset = urlParams.get("offset");
+    if (offset === null) {
+        offset = 0;
+    }
     $.ajax({
         url: `https://services.outsideworx.net/api/come-in-and-find-out/categories/${category}?offset=${offset}`,
         method: 'GET',
         success: function (response) {
             if (response && Array.isArray(response)) {
                 response.forEach((item, index) => {
-                    console.log("PROCESSING...");
                     if (item.image1) {
                         document.getElementById("image" + (index + 1)).src = item.image1;
                         const link = document.getElementById("item" + (index + 1));
@@ -27,4 +31,22 @@ function loadImages(category, offset) {
             console.error('Error fetching images:', error);
         }
     });
+}
+
+function setNavigation() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let offset = urlParams.get("offset");
+    if (offset) {
+        offset = parseInt(offset);
+        if (offset !== 0) {
+            let nav1 = document.getElementById("navigation1");
+            const url = new URL(nav1.href);
+            url.searchParams.set("offset", offset - 1);
+            nav1.href = url.toString();
+        }
+        let nav3 = document.getElementById("navigation3");
+        const url = new URL(nav3.href);
+        url.searchParams.set("offset", offset + 1);
+        nav3.href = url.toString();
+    }
 }

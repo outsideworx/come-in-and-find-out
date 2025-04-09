2557f8278904375b25a8707010b31f0359ed656d
hornokattila <hornokattila@outlook.com>
2025-04-09 14:10:06 +0200

    Setting auth token header.

--- functions/utils/proxy.ts
+++ functions/utils/proxy.ts
@@ -1,9 +1,14 @@
 export async function redirect(context: any, originUrl: string) {
-    const requestUrl = new URL(context.request.url);
+    const authToken = context.env.AUTH_TOKEN;
+    const headers = new Headers(context.request.headers);
+    if (authToken) {
+        headers.set("X-Auth-Token", authToken);
+    }
 
+    const requestUrl = new URL(context.request.url);
     const response = await fetch(originUrl + requestUrl.search, {
         method: context.request.method,
-        headers: context.request.headers,
+        headers: headers,
         body: context.request.method !== "GET" && context.request.method !== "HEAD"
             ? await context.request.text()
             : undefined,
